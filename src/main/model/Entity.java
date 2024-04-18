6ccb297b8e6ef8b12433432b6d3f3b52f6debf09
Fireboss_05 <47520445+Fireboss05@users.noreply.github.com>
2024-04-18 10:51:59 +0200

    entity move to not finished

--- src/main/model/Entity.java
+++ src/main/model/Entity.java
@@ -5,4 +5,28 @@ public abstract class Entity {
 	public void move(Direction dir) {
 		gj.move(this, dir);
 	}
+	
+	public void moveTo(Cell c , Direction dir) {
+		Entity e = c.getEntity();
+		if(e != null) {
+			e.push();
+		}
+		c.entrer(this.d);
+	}
+	
+	public static Entity loadEntity(char c) {
+		Entity res = null;
+		switch(c) {
+			case 'p':
+				res = new Player();
+				break;
+			case 'b':
+				res = new Box();
+				break;
+			default:
+				System.out.println("Erreur de charactere impossible");
+				break;
+		}
+		return res;
+	}
 }
