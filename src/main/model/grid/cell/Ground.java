ac2e0bab9e9904115d229e4a9ac4b06256a6e24a
Fireboss_05 <47520445+Fireboss05@users.noreply.github.com>
2024-05-13 15:11:55 +0200

    images

---
+++ src/main/model/grid/cell/Ground.java
@@ -0,0 +1,47 @@
+package main.model.grid.cell;
+
+import main.model.grid.Direction;
+import main.model.grid.GridGame;
+import main.model.entity.Entity;
+
+
+/**
+ * Represents a wall in the game.
+ * The wall is a cell that cannot be entered by any entity.
+ *
+ * @see Cell
+ */
+public class Ground extends Cell {
+
+	// === Variables ===
+
+
+
+	// === Constructors ===
+
+	/**
+	 * Constructor of the class Wall
+	 *
+	 * @param grid
+	 */
+	public Ground(GridGame grid) {
+		super(grid);
+		imagePath = "ground.png";
+		// TODO Auto-generated constructor stub
+	}
+
+
+	// === Methods ===
+
+	/**
+	 * Returns false because a wall cannot be entered.
+	 *
+	 * @param e the entity that wants to enter the cell
+	 * @param dir the direction from which the entity wants to enter the cell
+	 * @return false
+	 */
+	@Override
+	public boolean enter(Entity e, Direction dir) {
+		return false;
+	}
+}
