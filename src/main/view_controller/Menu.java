8dccfa0cba2c2924aa689a7a1ba0f44f1d00695c
Dorian <dorian.tonnis@hotmail.fr>
2024-05-22 18:55:45 +0200

    menu moins moche

--- src/main/view_controller/Menu.java
+++ src/main/view_controller/Menu.java
@@ -3,6 +3,7 @@ package main.view_controller;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Insets;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -12,11 +13,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import main.model.grid.GridGame;
 
@@ -36,12 +39,12 @@ public class Menu extends JFrame {
 	public Menu() {
 		try {
 			setTitle("PIEDS");
-
 			setLayout(new BorderLayout());
+			getContentPane().setBackground(new Color(19, 23, 30));
 
 			addTitle();
-
 			addLevelsButtons();
+			addControls();
 
 			setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 			pack();
@@ -58,14 +61,17 @@ public class Menu extends JFrame {
 	private void addTitle() {
 		// Create panel
 		JPanel titlePanel = new JPanel();
-		titlePanel.setPreferredSize(new Dimension(500, 64));
-		titlePanel.setBackground(Color.GREEN);
+		titlePanel.setOpaque(false);
+		titlePanel.setBorder(new EmptyBorder(20, 20, 20, 20));
 		add(titlePanel, BorderLayout.NORTH);
 
 		// Create title
 		JLabel titleLabel = new JLabel("Sélectionnez un niveau");
 		titleLabel.setFont(titleLabel.getFont().deriveFont(36.0f));
+		titleLabel.setForeground(Color.WHITE);
 		titlePanel.add(titleLabel);
+
+		pack();
 	}
 
 	/**
@@ -73,11 +79,21 @@ public class Menu extends JFrame {
 	 */
 	private void addLevelsButtons() {
 		JPanel panel = new JPanel();
+		panel.setOpaque(false);
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-		add(panel, BorderLayout.CENTER);
+		panel.setBorder(new EmptyBorder(0, 60, 20, 0));
+		add(panel, BorderLayout.WEST);
 
 		for (String level : getLevels()) {
 			JButton button = new JButton(level);
+
+			// Style
+			button.setContentAreaFilled(false);
+			button.setOpaque(true);
+			button.setBackground(new Color(41, 49, 65));
+			button.setForeground(Color.WHITE);
+
+			// Listener
 			button.addActionListener(e -> {
 				GridGame g = new GridGame("data/levels/" + level);
 				Frame f = new Frame(g, g.getWidth(), g.getHeight());
@@ -85,8 +101,41 @@ public class Menu extends JFrame {
 				f.setVisible(true);
 				dispose();
 			});
+
 			panel.add(button);
+			panel.add(Box.createVerticalStrut(2));
 		}
+
+		pack();
+	}
+
+	/**
+	 * Adds the controls to the menu.
+	 */
+	private void addControls() {
+		JPanel panel = new JPanel();
+		panel.setOpaque(false);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+		panel.setBorder(new EmptyBorder(0, 0, 20, 60));
+		add(panel, BorderLayout.EAST);
+
+		// Label which explain controls
+		JLabel label = new JLabel("<html>↑ ou Z : Haut<br>← ou Q : Gauche<br>↓ ou S : Bas<br>→ ou D : Droite<br>R : Recommencer</html>");
+		label.setFont(label.getFont().deriveFont(18.0f));
+		label.setForeground(Color.WHITE);
+		panel.add(label);
+
+		// Space
+		panel.add(Box.createRigidArea(new Dimension(0, 20)));
+
+		// Quit button
+		JButton button = new JButton("Quitter");
+		button.setContentAreaFilled(false);
+		button.setOpaque(true);
+		button.setBackground(new Color(41, 49, 65));
+		button.setForeground(Color.WHITE);
+		button.addActionListener(e -> dispose());
+		panel.add(button);
 
 		pack();
 	}
